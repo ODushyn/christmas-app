f2d49ac64bd6e2f17b43a7393260f11b1f454bd8
e211320 <oleksander.dushyn@evry.com>
2018-12-16 23:09:39 +0100

    update greetings config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { greetings } from './greetings-config.const';
 import { EmbedVideoService } from 'ngx-embed-video/dist';
 
@@ -8,25 +8,53 @@ import { EmbedVideoService } from 'ngx-embed-video/dist';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  passcode = 'core-renewal';
-  name: string;
+  passcode: string;
+  greeting: string;
+  bodyText: string;
   iframe_html: string;
+  error = false;
   authorized = false;
+  notChristYet = false;
 
 
   constructor(private embedVideoService: EmbedVideoService) {
 
   }
 
-
   passcodeSubmit() {
     const config = greetings[this.passcode];
     if (config) {
       this.authorized = true;
-      this.name = config.name;
-      this.iframe_html = this.embedVideoService.embed(config.video,
-        {query: {portrait: 0, color: '333'}, attr: {width: 760, height: 415}}
-      );
+      if (this.isChristmasAlready() || this.passcode === 'core-renewal') {
+        this.greeting = config.name;
+        this.iframe_html = this.embedVideoService.embed(config.video,
+          {query: {portrait: 0, color: '333'}, attr: {width: 760, height: 415}}
+        );
+      } else {
+        this.notChristYet = true;
+        if (config.lang === 'en') {
+          this.greeting = 'Hello ' + config.name;
+          this.bodyText = 'We would like to wish you Merry Christmas here. ' +
+            'However, Christmas has not come yet. Be patient and come back on December 25th! :)';
+        } else {
+          this.greeting = 'Привіт, ' + config.name;
+          this.bodyText = 'Ми хочемо привітати тут з Різдвом та Новий Роком! ' +
+            'Але Різдво то, ще не наступило... Трошки терпіння і чекаю назад 25 грудня! :)';
+        }
+      }
+    } else {
+      this.error = true;
     }
+  }
+
+  isChristmasAlready(): boolean {
+    const date = new Date();
+    return date.getFullYear() > 2018 || (date.getDate() > 24 && date.getMonth() === 11 && date.getFullYear() === 2018);
+  }
+
+  onMainPage() {
+    this.authorized = false;
+    this.notChristYet = false;
+    this.error = false;
   }
 }
--- src/app/greetings-config.const.ts
+++ src/app/greetings-config.const.ts
@@ -1,6 +1,96 @@
 export const greetings = {
   'core-renewal': {
     video: 'https://youtu.be/oS-LN88vfAM',
-    name: 'Core Renewal'
+    name: 'Merry Christmas, dear Core Renewal'
+  },
+  'ILNY': {
+    video: '',
+    name: 'Tanya and Vijay',
+    lang: 'en'
+  },
+  'TSNY': {
+    video: '',
+    name: 'Танюша',
+    lang: 'ua'
+  },
+  'DSNM': {
+    video: '',
+    name: 'Alina',
+    lang: 'en'
+  },
+  'CWAF': {
+    video: '',
+    name: 'Alyssa',
+    lang: 'en'
+  },
+  'YMNY': {
+    video: '',
+    name: 'Юля та Нікіта',
+    lang: 'ua'
+  },
+  'SNLY': {
+    video: '',
+    name: 'Альона, Ніка, Ніно та бабушка',
+    lang: 'ua'
+  },
+  'SKFC': {
+    video: '',
+    name: 'Катя',
+    lang: 'ua'
+  },
+  'DNDO': {
+    video: '',
+    name: 'Наталія Дмитріївна',
+    lang: 'ua'
+  },
+  'HMDP': {
+    video: '',
+    name: 'Мама та Тато',
+    lang: 'ua'
+  },
+  'SMVC': {
+    video: '',
+    name: 'Серьожа, тьотя Вєра, дядь Коля та баба Шура',
+    lang: 'ua'
+  },
+  'CTKJ': {
+    video: '',
+    name: 'бабушка Люба, дід Володя та дядя Андрій',
+    lang: 'ua'
+  },
+  'ETOL': {
+    video: '',
+    name: 'Єгорка та Танюшка Одуванчик',
+    lang: 'ua'
+  },
+  'NDOT': {
+    video: '',
+    name: 'Настьона',
+    lang: 'ua'
+  },
+  'ASSE': {
+    video: '',
+    name: 'Альона Зірка',
+    lang: 'ua'
+  },
+  'SINA': {
+    video: '',
+    name: 'Саньочек',
+    lang: 'ua'
+  },
+  'KVAB': {
+    video: '',
+    name: 'Антон та Васелина',
+    lang: 'ua'
+  },
+  'VKVR': {
+    video: '',
+    name: 'Валєрія та Васілій Рубан',
+    lang: 'ua'
+  },
+  'KIPB': {
+    video: '',
+    name: 'Інна',
+    lang: 'ua'
   }
 };
