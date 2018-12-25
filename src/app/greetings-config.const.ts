260b5668013ae08534896409286c433f725addf9
e211320 <oleksander.dushyn@evry.com>
2018-12-25 19:42:39 +0100

    update greetings

--- src/app/greetings-config.const.ts
+++ src/app/greetings-config.const.ts
@@ -6,97 +6,97 @@ export const greetings = {
   },
   'ILNY': {
     video: '',
-    name: 'Tanya and Vijay',
+    name: 'Tanya and Vijay, we wish you a Merry Christmas and Happy New Year',
     lang: 'en'
   },
   'TSNY': {
     video: '',
-    name: 'Танюша',
+    name: 'Танюша, вітаємо з Різдвом та Новим Роком',
     lang: 'ua'
   },
   'DSNM': {
     video: 'https://youtu.be/3I8nTjmNUHE',
-    name: 'Alina',
+    name: 'Аліна, Раян та Владислав Васильович вітаємо з Різдвом та Новим Роком',
     lang: 'en'
   },
   'CWAF': {
     video: '',
-    name: 'Alyssa',
+    name: 'Alyssa, we wish you a Merry Christmas and Happy New Year',
     lang: 'en'
   },
   'YMNY': {
     video: '',
-    name: 'Юля та Нікіта',
+    name: 'Юля та Нікіта, вітаємо з Різдвом та Новим Роком',
     lang: 'ua'
   },
   'SNLY': {
     video: 'https://youtu.be/t_NwbKOzzA8',
-    name: 'Альона, Ніка',
+    name: 'Альона, Ніка, вітаємо з Різдвом та Новим Роком',
     lang: 'ua'
   },
   'SKFC': {
     video: 'https://youtu.be/SarMXsrGkkQ',
-    name: 'Катя',
+    name: 'Катя, вітаємо з Різдвом та Новим Роком',
     lang: 'ua'
   },
   'DNDO': {
     video: 'https://youtu.be/gbJqFfvn8jU',
-    name: 'Наталія Дмитріївна',
+    name: 'Наталія Дмитріївна, вітаємо з Різдвом та Новим Роком',
     lang: 'ua'
   },
   'HMDP': {
     video: 'https://youtu.be/fICXxELnhI8',
-    name: 'Мама та Тато',
+    name: 'Мама та Тато, вітаємо з Різдвом та Новим Роком',
     lang: 'ua'
   },
   'SMVC': {
     video: 'https://youtu.be/8lBpFCVoCrY',
-    name: 'Серьожа, тьотя Вєра, дядь Коля та баба Шура',
+    name: 'Серьожа, тьотя Вєра, дядь Коля та баба Шура, вітаємо з Різдвом та Новим Роком',
     lang: 'ua'
   },
   'CTKJ': {
     video: 'https://youtu.be/BabHbiyJECE',
-    name: 'бабушка Люба, дід Володя та дядя Андрій',
+    name: 'бабушка Люба, дід Володя та дядя Андрій, вітаємо з Різдвом та Новим Роком',
     lang: 'ua'
   },
   'ETOL': {
     video: 'https://youtu.be/4mbHaV2ndtQ',
-    name: 'Єгорка та Танюшка Одуванчик',
+    name: 'Єгорка та Танюшка Одуванчик, вітаємо з Різдвом та Новим Роком',
     lang: 'ua'
   },
   'NDOT': {
     video: '',
-    name: 'Настьона',
+    name: 'Настьона, вітаємо з Різдвом та Новим Роком',
     lang: 'ua'
   },
   'ASSE': {
     video: '',
-    name: 'Альона Зірка',
+    name: 'Альона Зірка, вітаємо з Різдвом та Новим Роком',
     lang: 'ua'
   },
   'SINA': {
     video: 'https://youtu.be/nTT6Sk7PIxs',
-    name: 'Саньочек',
+    name: 'Саньочек, вітаємо з Різдвом та Новим Роком',
     lang: 'ua'
   },
   'KVAB': {
     video: 'https://youtu.be/qOlD3x2gXqg',
-    name: 'Антон',
+    name: 'Антон, вітаємо з Різдвом та Новим Роком',
     lang: 'ua'
   },
   'VKVR': {
     video: '',
-    name: 'Валєрія та Васілій Рубан',
+    name: 'Валєрія та Васілій Рубан, вітаємо з Різдвом та Новим Роком',
     lang: 'ua'
   },
   'KIPB': {
     video: 'https://youtu.be/qxMG2dMiqnA',
-    name: 'Інна',
+    name: 'Інна, вітаємо з Різдвом та Новим Роком',
     lang: 'ua'
   },
   'QWER': {
     video: 'https://youtu.be/eHWInd2qN80',
-    name: 'Богдан',
+    name: 'Богдан, вітаємо з Різдвом та Новим Роком',
     lang: 'ua'
   }
 };
